b2f475bdab47ada1b665a23a972352df83c97f80
Pavel Horáček <pahorace@redhat.com>
2020-06-26 23:50:22 +0200

    Forgot comments

--- shareit.py
+++ shareit.py
@@ -11,14 +11,7 @@ sh_path = home+"/.share/"
 if not os.path.exists(sh_path):
     os.mkdir(sh_path)
 
-#selinux_state = subprocess.Popen(["getenforce"], stdout=subprocess.PIPE).communicate()[0]
-#print(selinux_state[:9])
-
-#if selinux_state[:9] == "Enforced":
-#    print("Yep")
-
 hash_name=hashlib.md5(sys.argv[1].encode()).hexdigest()[:10]
-link_path=sh_path+hash_name
 if not os.path.exists("/var/www/html/share/"+hash_name):
     os.symlink(os.path.abspath(sys.argv[1]),"/var/www/html/share/"+hash_name)
 
